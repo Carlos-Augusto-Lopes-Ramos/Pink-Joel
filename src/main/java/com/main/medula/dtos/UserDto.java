bb7f0fb0b080231f5de88ef1974757b79b817934
Carlos-Augusto-Lopes-Ramos <123343900+Carlos-Augusto-Lopes-Ramos@users.noreply.github.com>
2025-12-17 13:24:30 -0300

    Update UserDto.java

--- src/main/java/com/main/medula/dtos/UserDto.java
+++ src/main/java/com/main/medula/dtos/UserDto.java
@@ -3,10 +3,10 @@ package com.main.medula.dtos;
 import com.main.medula.models.UsersModel;
 import org.antlr.v4.runtime.misc.NotNull;
 
-public record UserDto(@NotNull String email, @NotNull String name, @NotNull String pswrd, @NotNull boolean master) {
+public record UserDto(@NotNull String email, @NotNull String name, @NotNull String pswrd) {
 
     public UsersModel materialize() {
-        return new UsersModel(email, name, pswrd, master);
+        return new UsersModel(email, name, pswrd, false);
     }
 
 }
